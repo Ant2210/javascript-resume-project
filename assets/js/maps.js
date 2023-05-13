1e9edd6910db08e2a3f46c1fa582ace82662349f
Ant <ant.griffiths22@gmail.com>
2023-05-13 19:24:27 +0100

    Add google maps API

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -2,8 +2,8 @@ function initMap() {
     let map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
         center: {
-            lat: 46.619261,
-            lng: -33.134766,
+            lat: 40.090407,
+            lng: -98.285670,
         },
     });
     let labels = "ABCDEFGHIJKLLMNOPQRSTUVWXYZ";
